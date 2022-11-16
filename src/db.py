fe070d1295f206430a6eebe819e141adb2e0ac9d
Polly <98384702+cofffecup@users.noreply.github.com>
2022-11-16 20:34:48 +0300

    fixed auth

--- src/db.py
+++ src/db.py
@@ -4,24 +4,7 @@ import sqlite3
 def get_host_password():
     con = sqlite3.connect('quizbot.db')
     cursor = con.cursor()
-    tmp = "SELECT password FROM passwords WHERE user_type = \"host\""
-    try:
-        cursor.execute(tmp)
-    except sqlite3.IntegrityError:
-        cursor.close()
-        con.close()
-        return 0
-    else:
-        res = cursor.fetchall()
-    cursor.close()
-    con.close()
-    return res[0]
-
-
-def get_gamer_password():
-    con = sqlite3.connect('quizbot.db')
-    cursor = con.cursor()
-    tmp = "SELECT password FROM passwords WHERE user_type = \"gamer\""
+    tmp = "SELECT password FROM passwords WHERE user_type = 'host'"
     try:
         cursor.execute(tmp)
     except sqlite3.IntegrityError:
@@ -35,7 +18,7 @@ def get_gamer_password():
     return res[0]
 
 
-def get_host_chat_id():
+def get_gamer_password():
     con = sqlite3.connect('quizbot.db')
     cursor = con.cursor()
     tmp = "SELECT password FROM passwords WHERE user_type = \"gamer\""
@@ -86,6 +69,23 @@ def add_team_to_teams(name):
         return True
 
 
+def add_host_to_game(chat_id):
+    con = sqlite3.connect('quizbot.db')
+    cursor = con.cursor()
+    tmp = "INSERT INTO game_teams (chat_id, name, access) VALUES (" + str(chat_id) + ", 'HOST', 'host')"
+    try:
+        cursor.execute(tmp)
+    except sqlite3.IntegrityError:
+        cursor.close()
+        con.close()
+        return False
+    else:
+        con.commit()
+        cursor.close()
+        con.close()
+        return True
+
+
 def get_all_teams():
     con = sqlite3.connect('quizbot.db')
     cursor = con.cursor()
@@ -101,3 +101,37 @@ def get_all_teams():
     cursor.close()
     con.close()
     return res
+
+
+def get_host_id():
+    con = sqlite3.connect('quizbot.db')
+    cursor = con.cursor()
+    tmp = "SELECT chat_id FROM game_teams WHERE access = 'host'"
+    try:
+        cursor.execute(tmp)
+    except sqlite3.IntegrityError:
+        cursor.close()
+        con.close()
+        return 0
+    else:
+        res = cursor.fetchone()
+    cursor.close()
+    con.close()
+    return int(res[0]) if res else 0
+
+
+def get_access(chat_id):
+    con = sqlite3.connect('quizbot.db')
+    cursor = con.cursor()
+    tmp = "SELECT access FROM game_teams WHERE chat_id = '" + str(chat_id) + "'"
+    try:
+        cursor.execute(tmp)
+    except sqlite3.IntegrityError:
+        cursor.close()
+        con.close()
+        return 0
+    else:
+        res = cursor.fetchone()
+    cursor.close()
+    con.close()
+    return res[0] if res else ""
--- src/interface.py
+++ src/interface.py
@@ -4,7 +4,6 @@ import telebot
 
 
 bot = telebot.TeleBot("5629181435:AAFOBdRBGJT8tDnddW6kPnGHt5Mj12Y6J_E")
-access = ""
 authorization_flag = 0
 
 
@@ -15,19 +14,20 @@ def get_team_name(message):
     name.lower()
     if db.add_team_to_game(message.chat.id, name):
         if name in db.get_all_teams():
-            print(f"Команда {name} снова с нами!")
+            bot.send_message(db.get_host_id(), text=f"Команда {name} снова с нами!")
             bot.send_message(message.chat.id, text="Авторизация команды пройдена, рады снова видеть вас на нашей\
-                        игре!\n\nЕсли ваша команда до этого не участвовала в играх этого турнира, то сообщите ведущему\
-                        об ошибке.")
+            игре!\n\nЕсли ваша команда до этого не участвовала в играх этого турнира, то сообщите\
+            ведущему об ошибке")
         else:
             if db.add_team_to_teams(name):
-                print(f"Команда {name} добавлена в команды турнира")
+                bot.send_message(db.get_host_id(), text=f"Команда {name} добавлена в команды турнира")
             bot.send_message(message.chat.id, text="Авторизация команды пройдена, мы всегда рады новым командам!\
-                        \n\nЕсли ваша команда до этого уже участвовала в играх этого турнира, то сообщите ведущему об\
-                        ошибке.")
+            \n\nЕсли ваша команда до этого уже участвовала в играх этого турнира, то сообщите\
+            ведущему об ошибке")
         authorization_flag = 0
     else:
-        print(f"!!!!Команда {name} пользователя {message.from_user.username} не может быть добавлена!!!!")
+        bot.send_message(db.get_host_id(), text=f"!!!!Команда {name} пользователя {message.from_user.username} не может\
+        быть добавлена!!!!")
         bot.reply_to(message, text="Команда с таким названием уже есть в этой игре, пожалуйста, выберите другое:")
         authorization_flag = 1
 
@@ -41,15 +41,17 @@ def start(message):
 
 @bot.message_handler(content_types=['text'])
 def parser(message):
-    global access, authorization_flag
-    if access == "":
-        if message.text == db.get_host_password():
-            access = "host"
-            bot.send_message(message.chat.id, text="Авторизация ведущего игры пройдена, можете начинать.")
+    global authorization_flag
+    if db.get_access(message.chat.id) == "":
+        if message.text == db.get_host_password() or message.chat.id == db.get_host_id():
+            db.add_host_to_game(message.chat.id)
+            bot.send_message(message.chat.id, text="Авторизация ведущего игры пройдена, можете начинать")
         elif message.text == db.get_gamer_password():
-            access = "gamer"
-            bot.send_message(message.chat.id, text="Авторизация игрока пройдена. Введите название вашей команды:")
-            authorization_flag = bot.register_next_step_handler(message, get_team_name)
+            if db.get_host_id() != 0:
+                bot.send_message(message.chat.id, text="Авторизация игрока пройдена. Введите название вашей команды:")
+                bot.register_next_step_handler(message, get_team_name)
+            else:
+                bot.send_message(message.chat.id, text="Пожалуйста, дождитесь авторизации ведущего")
         else:
             bot.send_message(message.chat.id, text="Пароль введен неверно!\nПовторите ввод:")
     else:
